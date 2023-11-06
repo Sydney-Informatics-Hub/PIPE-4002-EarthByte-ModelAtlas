6a908307990dc79c7f7188d7173f5d7f744b8dae
Tim White <tim.white@sydney.edu.au>
2023-11-06 17:43:15 +1100

    debug issue number

--- .github/scripts/parse_issue.py
+++ .github/scripts/parse_issue.py
@@ -2,7 +2,7 @@ import os
 from github import Github, Auth
 
 token = os.environ.get("GITHUB_TOKEN")
-issue_number = os.environ.get("ISSUE_NUMBER")
+issue_number = int(os.environ.get("ISSUE_NUMBER"))
 
 print("ISSUE_NUMBER:", issue_number)
 
@@ -11,8 +11,6 @@ auth = Auth.Token(token)
 g = Github(auth=auth)
 
 repo = g.get_repo('Sydney-Informatics-Hub/PIPE-4002-EarthByte-ModelAtlas')
-
-print(repo.name)
 
 issue = repo.get_issue(number = issue_number)
 
