94e2b95ec625550bdfaf527cb8de12b9a6317b99
Tim White <tim.white@sydney.edu.au>
2023-11-09 19:02:33 +1100

    Hack to recognise svg files

--- .github/scripts/parse_issue.py
+++ .github/scripts/parse_issue.py
@@ -27,15 +27,33 @@ matches = re.findall(regex, issue.body)
 regex = r"\[(?P<filename>.*?)\]\((?P<url>.*?)\)"
 file_matches = re.findall(regex, issue.body)
 
+# Hack to recognise SVG files
+filetype.add_type(Svg())
+
 # Download files and move them to the correct location in the repo
 for filename, url in file_matches:
 	response = requests.get(url)
 
 	# Image file extensions are left out, infer what they should be
 	if response.headers.get('Content-Type')[0:5] == 'image':
-		filename += '.'+filetype.guess(response.content).extension
+		filename += '.'+filetype.get_type(mime=response.headers.get('Content-Type')).extension
 
-	repo.create_file("pages/models/"+slug+filename,"add "+filename,response.content)
+	repo.create_file("pages/models/"+slug+"/"+filename,"add "+filename,response.content)
 
 
 print(issue.body)
+
+
+# SVG filetype class for hack
+class Svg(filetype.Type):
+	MIME = 'image/svg+xml'
+	EXTENSION = 'svg'
+
+	def __init__(self):
+		super(Svg, self).__init__(
+			mime = Svg.MIME,
+			extension = Svg.EXTENSION
+			)
+
+	def match(self, buf):
+		return False
