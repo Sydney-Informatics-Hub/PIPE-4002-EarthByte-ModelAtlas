58b8d06619b78199ab7ca1d8f799561577f5d821
Tim White <tim.white@sydney.edu.au>
2023-12-19 15:59:45 +1100

    Fix formatting

--- .github/scripts/parse_issue.py
+++ .github/scripts/parse_issue.py
@@ -32,23 +32,23 @@ parse_log += '**Creator/Contributor**\n'
 creator = data["-> creator/contributor ORCID (or name)"].strip()
 
 if is_orcid_format(creator):
-	orcid_record = get_record("author", creator)
-	creator_record, log = parse_author(orcid_record)
-	if log:
-		parse_log += log
-	else:
-		parse_log += f"Creator/contributor is {creator_record['givenName']} {creator_record['familyName']} ({creator_record['@id']})\n"
+    orcid_record = get_record("author", creator)
+    creator_record, log = parse_author(orcid_record)
+    if log:
+        parse_log += log
+    else:
+        parse_log += f"Creator/contributor is {creator_record['givenName']} {creator_record['familyName']} ({creator_record['@id']})\n"
 else:
     try:
-	    familyName, givenName = author.split(",")
-    	creator_record = {
-        	"@type": "Person",
-        	"givenName": givenName,
-        	"familyName": familyName,
-    	}
-    	parse_log += f"Creator/contributor is {creator_record['givenName']} {creator_record['familyName']}\n"
-	except:
-    	log += f"- Error: creator name `{author}` in unexpected format. Expected `last name(s), first name(s)` or ORCID. \n"
+        familyName, givenName = author.split(",")
+        creator_record = {
+            "@type": "Person",
+            "givenName": givenName,
+            "familyName": familyName,
+        }
+        parse_log += f"Creator/contributor is {creator_record['givenName']} {creator_record['familyName']}\n"
+    except:
+        log += f"- Error: creator name `{author}` in unexpected format. Expected `last name(s), first name(s)` or ORCID. \n"
 
 parse_log += "\n"
 
@@ -60,15 +60,15 @@ proposed_slug = data["-> slug"].strip()
 os.environ['ISSUE_NAME'] = proposed_slug
 cmd = "python3 .github/scripts/generate_identifier.py"
 try:
-	slug = subprocess.check_output(cmd, shell=True, text=True, stderr=open(os.devnull)).strip()
-	if proposed_slug != slug:
-		parse_log += f"Warning: Model repo cannot be created with proposed slug `{proposed_slug}`. \n"
-		parse_log += f"Either propose a new slug or repo will be created with name `{slug}`. \n" 
-	else:
-		parse_log += f"Model repo will be created with name `{slug}` \n"
+    slug = subprocess.check_output(cmd, shell=True, text=True, stderr=open(os.devnull)).strip()
+    if proposed_slug != slug:
+        parse_log += f"Warning: Model repo cannot be created with proposed slug `{proposed_slug}`. \n"
+        parse_log += f"Either propose a new slug or repo will be created with name `{slug}`. \n" 
+    else:
+        parse_log += f"Model repo will be created with name `{slug}` \n"
 except Exception as err:
-	parse_log += "- Unable to create valid repo name... \n"
-	parse_log += f"`{err}`\n"
+    parse_log += "- Unable to create valid repo name... \n"
+    parse_log += f"`{err}`\n"
 
 parse_log += "\n"
 
@@ -87,18 +87,18 @@ parse_log += "**Associated Publication**\n"
 publication_doi = data["-> associated publication DOI"].strip()
 
 if publication == "_No response_":
-	parse_log += "No DOI provided. \n"
+    parse_log += "No DOI provided. \n"
 else:
-	try:
-		publication_metadata = get_record("publication", publication_doi)
-		publication_record, log = parse_publication(publication_metadata)
-		if log:
-			parse_log += log
-		else:
-			parse_log += f"Found publication: _{publication_record['name']}_. \n"
-	except Exception as err:
-		parse_log += f"- Error: unable to obtain metadata for DOI {publication} \n"
-		parse_log += f"`{err}`\n"
+    try:
+        publication_metadata = get_record("publication", publication_doi)
+        publication_record, log = parse_publication(publication_metadata)
+        if log:
+            parse_log += log
+        else:
+            parse_log += f"Found publication: _{publication_record['name']}_. \n"
+    except Exception as err:
+        parse_log += f"- Error: unable to obtain metadata for DOI {publication} \n"
+        parse_log += f"`{err}`\n"
 
 parse_log += "\n"
 
@@ -108,11 +108,11 @@ parse_log += "**Title**\n"
 title = data["-> title"].strip()
 
 if title == "_No response_":
-	try:
-		title = publication_record['name']
-		parse_log += "_Title taken from associated publication_ \n"
-	else:
-		parse_log += "- Error: no title found \n"
+    try:
+        title = publication_record['name']
+        parse_log += "_Title taken from associated publication_ \n"
+    else:
+        parse_log += "- Error: no title found \n"
 
 parse_log += title + "\n \n"
 
@@ -122,11 +122,11 @@ parse_log += "**Description**\n"
 description = data["-> description"].strip()
 
 if description == "_No response_":
-	try:
-		title = publication_record['abstract']
-		parse_log += "_Description taken from associated publication abstract_ \n"
-	else:
-		parse_log += "- Error: no descrition found, nor abstract for associated publication \n"
+    try:
+        title = publication_record['abstract']
+        parse_log += "_Description taken from associated publication abstract_ \n"
+    else:
+        parse_log += "- Error: no descrition found, nor abstract for associated publication \n"
 
 parse_log += description + "\n \n"
 
@@ -137,22 +137,22 @@ parse_log += "**Model authors**\n"
 authors = data['-> model authors'].strip().split('\r\n')
 
 if authors[0] == "_No response_":
-	try:
-		author_list = publication_record['author']
-		parse_log += "_Author list taken from associated publication_ \n"
-	else:
-		parse_log += "- Error: no authors found \n"
+    try:
+        author_list = publication_record['author']
+        parse_log += "_Author list taken from associated publication_ \n"
+    else:
+        parse_log += "- Error: no authors found \n"
 else:
-	author_list, log = get_authors(authors)
-	parse_log += log
+    author_list, log = get_authors(authors)
+    parse_log += log
 
 parse_log += "\n"
 parse_log += "The following author(s) were found successfully:\n"
 for author in author_list:
-	if "@id" in author:
-		parse_log += f"- {author['givenName']} {author['familyName']} ({author['@id']})\n"
-	else:
-		parse_log += f"- {author['givenName']} {author['familyName']}\n"
+    if "@id" in author:
+        parse_log += f"- {author['givenName']} {author['familyName']} ({author['@id']})\n"
+    else:
+        parse_log += f"- {author['givenName']} {author['familyName']}\n"
 parse_log += "\n"
 
 
@@ -165,21 +165,21 @@ parse_log += "**Funder**\n"
 funders = data['-> funder'].strip().split('\r\n')
 
 if funders[0] == "_No response_":
-	try:
-		funders = publication_record['funder']
-		parse_log += "_Funder list taken from associated publication_ \n"
-	else:
-		parse_log += "- Warning: No funders provided or found in publication. \n"
+    try:
+        funders = publication_record['funder']
+        parse_log += "_Funder list taken from associated publication_ \n"
+    else:
+        parse_log += "- Warning: No funders provided or found in publication. \n"
 else:
-	print("Still need to code this bit")
-	funders = []
-	# for funder in funders:
-	# 	funder_list.append({"@type": "Organization", "name": funder})
+    print("Still need to code this bit")
+    funders = []
+    # for funder in funders:
+    #   funder_list.append({"@type": "Organization", "name": funder})
 
 parse_log += "\n"
 parse_log += "The following funder(s) were found successfully:\n"
 for funder in funders:
-	parse_log += f"- {funder['name']} \n"
+    parse_log += f"- {funder['name']} \n"
 parse_log += "\n"
 
 
@@ -196,32 +196,32 @@ file_matches = re.findall(regex, issue.body)
 # Hack to recognise SVG files
 # Need to tidy this up, and are there any other files that filetype doesn't natively recognise?
 class Svg(filetype.Type):
-	MIME = 'image/svg+xml'
-	EXTENSION = 'svg'
+    MIME = 'image/svg+xml'
+    EXTENSION = 'svg'
 
-	def __init__(self):
-		super(Svg, self).__init__(
-			mime = Svg.MIME,
-			extension = Svg.EXTENSION
-			)
+    def __init__(self):
+        super(Svg, self).__init__(
+            mime = Svg.MIME,
+            extension = Svg.EXTENSION
+            )
 
-	def match(self, buf):
-		return False
+    def match(self, buf):
+        return False
 
 filetype.add_type(Svg())
 
 filenames = []
 # Download files and move them to the correct location in the repo
 for filename, url in file_matches:
-	response = requests.get(url)
+    response = requests.get(url)
 
-	# Image file extensions are left out, infer what they should be
-	if response.headers.get('Content-Type')[0:5] == 'image':
-		filename += '.'+filetype.get_type(mime=response.headers.get('Content-Type')).extension
+    # Image file extensions are left out, infer what they should be
+    if response.headers.get('Content-Type')[0:5] == 'image':
+        filename += '.'+filetype.get_type(mime=response.headers.get('Content-Type')).extension
 
-	# repo.create_file("pages/models/"+slug+"/"+filename,"add "+filename,response.content)
-	parse_log += filename + " | \n"
-	filenames.append(filename)
+    # repo.create_file("pages/models/"+slug+"/"+filename,"add "+filename,response.content)
+    parse_log += filename + " | \n"
+    filenames.append(filename)
 
 # Test making a comment - could this be edited later for the uploader to give file descriptions?
 
