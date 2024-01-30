5fc7d95702c8a02d7d3b45eb4ea7982dd9b51bea
Tim White <tim.white@sydney.edu.au>
2024-01-30 15:59:27 +1100

    Rename metadata_utils to parse_metadata_utils, move other functions to parse_utils

--- .github/scripts/parse_metadata_utils.py
+++ .github/scripts/parse_metadata_utils.py
@@ -5,7 +5,6 @@ import filetype
 from filetypes import Svg
 
 from request_utils import get_record, search_organization
-from parse_utils import parse_name_or_orcid
 
 
 def parse_author(metadata):
@@ -171,106 +170,4 @@ def parse_publication(metadata):
 
     return publication_record, log
 
-
-def is_orcid_format(author):
-
-    orcid_pattern = re.compile(r'\d{4}-\d{4}-\d{4}-\d{3}[0-9X]')
-
-    if orcid_pattern.fullmatch(author):
-        return True
-    else:
-        return False
-
-
-def get_authors(author_list):
-    '''
-    Parses a list of author names or ORCID iDs and returns a list of dictionaries of schema.org Person type
-
-        Parameters:
-            author_list (list of strings): list of names in format Last Name(s), First Name(s) and/or ORCID iDs
-
-        Returns:
-            authors (list of dicts)
-            log (string)
-
-    '''
-
-    log = ""
-    authors = []
-
-    for author in author_list:
-        author_record, error_log = parse_name_or_orcid(author)
-        if author_record:
-            authors.append(author_record)
-        if error_log:
-            log += error_log
-
-    return authors, log
-
-
-def get_funders(funder_list):
-
-    log = ""
-    funders = []
-
-    for funder in funder_list:
-        if "ror.org" not in funder:
-            ror_id, get_log = search_organization(funder)
-            log += get_log
-
-            if not ror_id:
-                funders.append({"@type": "Organization", "name": funder, "url": funder})
-            else:
-                funder = ror_id
-
-        if "ror.org" in funder:
-            record, get_log = get_record("organization", funder)
-            funder_record, parse_log = parse_organization(record)
-            if get_log or parse_log:
-                log += get_log + parse_log
-            else:
-                funders.append(funder_record)
-
-    return funders, log
-
-
-def parse_image_and_caption(img_string, default_filename):
-    log = ""
-    image_record = {}
-    
-    md_regex = r"\[(?P<filename>.*?)\]\((?P<url>.*?)\)"
-    html_regex = r'alt="(?P<filename>[^"]+)" src="(?P<url>[^"]+)"'
-
-    # Hack to recognise SVG files
-    filetype.add_type(Svg())
-
-    caption = []
-
-    for string in img_string.split("\r\n"):
-        if "https://" in string:
-            try:
-                image_record = re.search(md_regex, string).groupdict()
-            except:
-                if string.startswith("https://"):
-                    image_record = {"filename": default_filename, "url": string}
-                elif "src" in string:
-                    image_record = re.search(html_regex, string).groupdict()
-                else:
-                    log += "Error: Could not parse image file and caption\n"
-        else:
-            caption.append(string)
-
-    # Get correct file extension for images
-    if "url" in image_record:
-        response = requests.get(image_record["url"])
-        content_type = response.headers.get("Content-Type")[:5]
-        if content_type in ["video", "image"]:
-            image_record["filename"] += "." + filetype.get_type(mime=response.headers.get("Content-Type")).extension
-
-    image_record["caption"] = "\n".join(caption)
-
-    if not caption:
-        log += "Error: No caption found for image.\n"
-
-    return image_record, log
 
--- .github/scripts/parse_utils.py
+++ .github/scripts/parse_utils.py
@@ -4,8 +4,8 @@ import json
 import pandas as pd
 import subprocess
 
-from request_utils import get_record, check_uri
-from metadata_utils import get_authors, get_funders, parse_author, parse_publication, parse_software, is_orcid_format, parse_image_and_caption
+from request_utils import get_record, check_uri, search_organization
+from parse_metadata_utils import parse_author, parse_publication, parse_software, parse_organization
 
 def parse_name_or_orcid(name_or_orcid):
     error_log = ""
@@ -38,6 +38,109 @@ def parse_yes_no_choice(input):
         return False
     else:
         return "Error: no selection made"
+
+def is_orcid_format(author):
+
+    orcid_pattern = re.compile(r'\d{4}-\d{4}-\d{4}-\d{3}[0-9X]')
+
+    if orcid_pattern.fullmatch(author):
+        return True
+    else:
+        return False
+
+
+def get_authors(author_list):
+    '''
+    Parses a list of author names or ORCID iDs and returns a list of dictionaries of schema.org Person type
+
+        Parameters:
+            author_list (list of strings): list of names in format Last Name(s), First Name(s) and/or ORCID iDs
+
+        Returns:
+            authors (list of dicts)
+            log (string)
+
+    '''
+
+    log = ""
+    authors = []
+
+    for author in author_list:
+        author_record, error_log = parse_name_or_orcid(author)
+        if author_record:
+            authors.append(author_record)
+        if error_log:
+            log += error_log
+
+    return authors, log
+
+
+def get_funders(funder_list):
+
+    log = ""
+    funders = []
+
+    for funder in funder_list:
+        if "ror.org" not in funder:
+            ror_id, get_log = search_organization(funder)
+            log += get_log
+
+            if not ror_id:
+                funders.append({"@type": "Organization", "name": funder, "url": funder})
+            else:
+                funder = ror_id
+
+        if "ror.org" in funder:
+            record, get_log = get_record("organization", funder)
+            funder_record, parse_log = parse_organization(record)
+            if get_log or parse_log:
+                log += get_log + parse_log
+            else:
+                funders.append(funder_record)
+
+    return funders, log
+
+
+def parse_image_and_caption(img_string, default_filename):
+    log = ""
+    image_record = {}
+    
+    md_regex = r"\[(?P<filename>.*?)\]\((?P<url>.*?)\)"
+    html_regex = r'alt="(?P<filename>[^"]+)" src="(?P<url>[^"]+)"'
+
+    # Hack to recognise SVG files
+    filetype.add_type(Svg())
+
+    caption = []
+
+    for string in img_string.split("\r\n"):
+        if "https://" in string:
+            try:
+                image_record = re.search(md_regex, string).groupdict()
+            except:
+                if string.startswith("https://"):
+                    image_record = {"filename": default_filename, "url": string}
+                elif "src" in string:
+                    image_record = re.search(html_regex, string).groupdict()
+                else:
+                    log += "Error: Could not parse image file and caption\n"
+        else:
+            caption.append(string)
+
+    # Get correct file extension for images
+    if "url" in image_record:
+        response = requests.get(image_record["url"])
+        content_type = response.headers.get("Content-Type")[:5]
+        if content_type in ["video", "image"]:
+            image_record["filename"] += "." + filetype.get_type(mime=response.headers.get("Content-Type")).extension
+
+    image_record["caption"] = "\n".join(caption)
+
+    if not caption:
+        log += "Error: No caption found for image.\n"
+
+    return image_record, log
+
 
 def parse_issue(issue):
     error_log = ""
