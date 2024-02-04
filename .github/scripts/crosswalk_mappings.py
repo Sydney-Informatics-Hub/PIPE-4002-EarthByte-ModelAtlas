371c98e06cde9b1c98db7a8012e3207a3cba4302
dansand <sonderfjord@gmail.com>
2024-02-05 09:36:18 +1100

    some changes to the mapping between the issue dict. and the ro-crate entities. Can now map a list of of keys...

--- .github/scripts/crosswalk_mappings.py
+++ .github/scripts/crosswalk_mappings.py
@@ -1,7 +1,9 @@
-#provides a mapping between keys in metadata and keys in issue dictionary (which are the values in the mapping)
-#None value indicate default values or properties which we not attempt to automatically fill 
+#provides a mapping between keys in metadata (left)
+#and keys in issue dictionary (which are the values in the mapping)
+#None value indicate default values or properties which we not attempt to automatically fill
+#this needs to refactored so that it looks for the approapriate @id
 
-root_node_mapping = {"@id":None,
+root_node_mapping = {"@id":"./",
             "@type":None,
             "name":"slug",
             "description":"description",
@@ -21,9 +23,9 @@ root_node_mapping = {"@id":None,
             "Dataset status":None,
             "hasPart":None
             }
-        
 
-model_inputs_node_mapping = {"@id":None,
+
+model_inputs_node_mapping = {"@id":"model_inputs",
             "@type":None,
             "description":None,
             "creator":"creator",
@@ -38,36 +40,37 @@ model_inputs_node_mapping = {"@id":None,
             "storageRequirements":None}
 
 
-model_outputs_node_mapping = {"@id":None,
-            "@id":None,
+model_outputs_node_mapping = {"@id":"model_outputs",
             "@type":None,
             "description":None,
             "creator":"creator",
             "author":None,
             "version":None,
             "programmingLanguage":None,
-            "owl:sameAs":None,
-            "keywords":None,
-            "runtimePlatform":None,
-            "memoryRequirements":None,
-            "processorRequirements":None,
-            "storageRequirements":None,
+            "fileFormat":None,
+            }
+
+website_material_node_mapping = {"@id":"website_material",
+            "@type":None,
+            "description":None,
+            "creator":"creator",
+            "author":None,
+            "fileFormat":None
             }
 
 
-
-dataset_creation_node_mapping = {"@id":None,
+dataset_creation_node_mapping = {"@id":"#datasetCreation",
             "@type":None,
             "agent":"creator",
             "description":None,
             "endTime":None,
             "endTime":None,
-            "instrument":"computer_uri",
+            "instrument":["software", "computer_uri"],
             "object":None,
             "result":None}
 
 default_issue_entity_mapping_list = [root_node_mapping,
                 model_inputs_node_mapping,
-                model_inputs_node_mapping,
+                model_outputs_node_mapping,
+                website_material_node_mapping,
                 dataset_creation_node_mapping]
-
